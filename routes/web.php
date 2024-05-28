1aeaa8917719df26d407cd52f8ac2141834f2b9b
Nasywan Muhammad Nuh <nasywan.idnsolo@gmail.com>
2024-05-28 08:14:44 +0700

    adding files

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+use App\Http\Controllers\DetailController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/categories', [App\Http\Controllers\CategoryController::class, 'index'])->name('categories');
+Route::get('/detail/(id)', [App\Http\Controllers\DetailController::class, 'index'])->name('detail');
+
+Auth::routes();
